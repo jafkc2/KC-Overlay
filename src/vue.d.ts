4be9053f0415a4abe0e2fa6497baf19e1278611d
jafkc2 <jaf222006ar@gmail.com>
2025-03-12 19:55:16 -0300

    Remove arquivos desnecessários

--- src/vue.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'vue' {
-  import { DefineComponent } from '@vue/runtime-core'
-  export * from '@vue/runtime-core'
-  export const createApp: any
-  export const ref: any
-  export const onMounted: any
-}
